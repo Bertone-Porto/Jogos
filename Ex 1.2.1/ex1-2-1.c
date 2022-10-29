b12248673232857c962d7a3519f46ab06b01e071
Bertone-Porto <38756105+Bertone-Porto@users.noreply.github.com>
2022-10-29 15:18:39 -0300

    Update ex1-2-1.c

--- Ex 1.2.1/ex1-2-1.c
+++ Ex 1.2.1/ex1-2-1.c
@@ -1,5 +1,4 @@
-
-// #include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
 int main (int argc, char* args[])
 {
     /* INICIALIZACAO */
@@ -15,34 +14,54 @@ int main (int argc, char* args[])
     SDL_SetRenderDrawColor(ren, 255,255,255,0x00);
     SDL_RenderClear(ren);
 
-//olhos
+	//olhos
     SDL_SetRenderDrawColor(ren, 163,255,127,0x00);
     SDL_Rect r1 = { 60,20,10,10 };
     SDL_RenderFillRect(ren, &r1);
-SDL_Rect r3 = { 100,20,10,10 };
-SDL_RenderFillRect(ren, &r3);
+	SDL_Rect r3 = { 100,20,10,10 };
+	SDL_RenderFillRect(ren, &r3);
 
-//nariz
-SDL_SetRenderDrawColor(ren, 60,40,0,0x00);
-SDL_Rect r2 = { 80,40,12,12 };
-SDL_RenderFillRect(ren, &r2);
+	//nariz
+	SDL_SetRenderDrawColor(ren, 60,40,0,0x00);
+	SDL_Rect r2 = { 80,40,12,12 };
+	SDL_RenderFillRect(ren, &r2);
 
-//boca
-SDL_SetRenderDrawColor(ren, 200,124,139,0x00);
-SDL_Rect r4 = { 50,50,10,10 };
-SDL_RenderFillRect(ren, &r4);
-SDL_Rect r5 = { 60,60,10,10 };
-SDL_RenderFillRect(ren, &r5);
-SDL_Rect r6 = { 70,60,10,10 };
-SDL_RenderFillRect(ren, &r6);
-SDL_Rect r7 = { 80,70,10,10 };
-SDL_RenderFillRect(ren, &r7);
-SDL_Rect r8 = { 90,60,10,10 };
-SDL_RenderFillRect(ren, &r8);
-SDL_Rect r9 = { 100,60,10,10 };
-SDL_RenderFillRect(ren, &r9);
-SDL_Rect r10 = { 110,50,10,10 };
-SDL_RenderFillRect(ren, &r10);
+	//boca
+	SDL_SetRenderDrawColor(ren, 200,124,139,0x00);
+	SDL_Rect r4 = { 50,50,10,10 };
+	SDL_RenderFillRect(ren, &r4);
+	SDL_Rect r5 = { 60,60,10,10 };
+	SDL_RenderFillRect(ren, &r5);
+	SDL_Rect r6 = { 70,60,10,10 };
+	SDL_RenderFillRect(ren, &r6);
+	SDL_Rect r7 = { 80,70,10,10 };
+	SDL_RenderFillRect(ren, &r7);
+	SDL_Rect r8 = { 90,60,10,10 };
+	SDL_RenderFillRect(ren, &r8);
+	SDL_Rect r9 = { 100,60,10,10 };
+	SDL_RenderFillRect(ren, &r9);
+	SDL_Rect r10 = { 110,50,10,10 };
+	SDL_RenderFillRect(ren, &r10);
+
+	//bigode
+	SDL_SetRenderDrawColor(ren, 0,0,0,0x00);
+	SDL_RenderDrawLine(ren,20,35,70,40);
+	SDL_RenderDrawLine(ren,18,38,70,42);
+	SDL_RenderDrawLine(ren,20,41,70,44);
+
+	SDL_RenderDrawLine(ren,160,35,110,40);
+	SDL_RenderDrawLine(ren,158,38,110,42);
+	SDL_RenderDrawLine(ren,160,41,110,44);
+
+
+    SDL_RenderPresent(ren);
+    SDL_Delay(8000);
+
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
 
 //bigode
 SDL_SetRenderDrawColor(ren, 0,0,0,0x00);
