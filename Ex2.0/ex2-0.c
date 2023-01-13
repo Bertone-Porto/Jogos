d23478ca7781e2dd078b89d0d5c98614f45c3c5a
Bertone-Porto <38756105+Bertone-Porto@users.noreply.github.com>
2023-01-13 19:02:25 -0300

    Update ex2-0.c

--- Ex2.0/ex2-0.c
+++ Ex2.0/ex2-0.c
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 
+enum Estado{READY, CANCELADO, CLICADO, SOLTO, ARRASTANDO, CLICANDO};
+
+
 int AUX_WaitEventTimeoutCount (SDL_Event* evt, Uint32* ms){
 	Uint32 antes = SDL_GetTicks();
 	int isevt = SDL_WaitEventTimeout(evt, *ms);
@@ -30,117 +33,102 @@ int main (int argc, char* args[])
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
     /* EXECUÇÃO */
-	bool rodando=true, selecionado=false, cancel=false, arrasta=false, clicado=false, iniciado=false, escape=false, arrastado=false, solto=false;
-	int posX=200, posY=200, x, y, xAnterior=0, yAnterior=0;
+	int estado_atual = 0;
+
+
+	bool rodando=true, selecionado=false;
+	int posX=200, posY=200, x, y, xAnterior, yAnterior;
 	SDL_Event evt;
 	SDL_Rect r1 = { posX, posY, 80,80 };
-	int espera = 10, vencedor=0, dx, dy;
+	int espera = 10, dx, dy, novoX, novoY;
+	char* texto[] = {"ready", "cancelado", "clicado", "solto", "arrastando...",""};
+
 	while (rodando) {
-	SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-	SDL_RenderClear(ren);
+		SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+		SDL_RenderClear(ren);
 
-	SDL_SetRenderDrawColor(ren, 250, 0, 0, 250);
-	SDL_RenderFillRect(ren, &r1);
+		SDL_SetRenderDrawColor(ren, 250, 0, 0, 250);
+		SDL_RenderFillRect(ren, &r1);
+		
+		//MENSAGENS
 
-	//MENSAGENS
-	if(!iniciado){
-		stringRGBA(ren, 20,20,"ready", 250,0,0,255);
-	}
-	else if(arrasta){
-		stringRGBA(ren, 20,20,"arrastando", 250,0,0,255);
-	}
-	else if(solto){
-		stringRGBA(ren, 20,20,"solto", 250,0,0,255);
-	}
-	else if(clicado && !arrasta){
-		stringRGBA(ren, 20,20,"clicado", 250,0,0,255);
-	}
-	else if(cancel){
-		stringRGBA(ren, 20,20,"cancelado", 250,0,0,255);
-	}
+		stringRGBA(ren, 20, 20, texto[estado_atual], 250,0,0,255);
 
-	SDL_RenderPresent(ren);
+		SDL_RenderPresent(ren);
 
-	SDL_Event evt;
-	Uint32 antes = SDL_GetTicks();
-	int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
-	if (isevt) {
-		espera -= (SDL_GetTicks() - antes);
-		if (espera < 0) {
-			espera = 0;
-		}
-	 
-		//ESCAPE//
-		if (evt.type == SDL_KEYDOWN && arrasta) {
-			switch (evt.key.keysym.sym) {
-				case SDLK_ESCAPE:
-					r1.x = xAnterior;
-					r1.y = yAnterior;
-					selecionado = false;
-					arrasta = false;
-					cancel = true;
-					solto = false;
-				break;
+		SDL_Event evt;
+		Uint32 antes = SDL_GetTicks();
+		int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
+		if (isevt) {
+			espera -= (SDL_GetTicks() - antes);
+			if (espera < 0) {
+				espera = 0;
 			}
-		}
-
-		//CLICK//
-		if(evt.type == SDL_MOUSEBUTTONDOWN){
-			if(evt.button.button == SDL_BUTTON_LEFT){
-				SDL_GetMouseState(&x, &y);
-				if((x > r1.x) && (x < r1.x + r1.w) && (y > r1.y) && (y < r1.y + r1.h)){
-				selecionado = true;
-				iniciado = true;
-				escape = false;
-				solto = false;
-				dx = r1.x - x;
-				dy = r1.y - y;
+		 
+			//ESCAPE//
+			if (evt.type == SDL_KEYDOWN && estado_atual == ARRASTANDO) {
+				switch (evt.key.keysym.sym) {
+					case SDLK_ESCAPE:
+						estado_atual = CANCELADO;
+						r1.x = xAnterior;
+						r1.y = yAnterior;
+						selecionado = false;
+					break;
 				}
 			}
-		}
 
-		//LIBERA//
-		if(evt.type == SDL_MOUSEBUTTONUP){
-			if(evt.button.button == SDL_BUTTON_LEFT){
-				SDL_GetMouseState(&x, &y);
-					if(evt.button.state == SDL_RELEASED){
-						if((x > r1.x) && (x < r1.x + r1.w) && (y > r1.y) && (y < r1.y + r1.h)){
-						xAnterior = posX;
-						yAnterior = posY;
-						selecionado = false;
-						if(arrasta){
-							arrasta = false;
-							solto = true;
-							
-						}
-						cancel = false;
-						clicado = true;
-						
-						
+			//CLICK//
+			if(evt.type == SDL_MOUSEBUTTONDOWN){
+				if(evt.button.button == SDL_BUTTON_LEFT){
+					SDL_GetMouseState(&x, &y);
+					if((x > r1.x) && (x < r1.x + r1.w) && (y > r1.y) && (y < r1.y + r1.h)){
+						estado_atual = CLICANDO;
+						selecionado = true;
+						dx = r1.x - x;
+						dy = r1.y - y;
 					}
 				}
 			}
-		}
 
-		//ARRASTA//
-		if(evt.type == SDL_MOUSEMOTION){
-			SDL_GetMouseState(&x,&y);
-			if(r1.x <= 460  && r1.y <= 460 && selecionado){
-				arrasta = true;
-				clicado = false;
-				solto=false;
-				posX = evt.button.x + dx;
-				posY = evt.button.y + dy;
-				r1.x = posX ;
-				r1.y = posY ;
+			//LIBERA//
+			if(evt.type == SDL_MOUSEBUTTONUP){
+				if(evt.button.button == SDL_BUTTON_LEFT){
+					SDL_GetMouseState(&x, &y);
+						if(evt.button.state == SDL_RELEASED){
+							if((x > r1.x) && (x < r1.x + r1.w) && (y > r1.y) && (y < r1.y + r1.h)){
+								if(estado_atual = CLICANDO){
+									estado_atual = CLICADO;
+
+								} else if(estado_atual == ARRASTANDO){
+									estado_atual = SOLTO;
+								}			
+								xAnterior = posX;
+								yAnterior = posY;
+								selecionado = false;
+							}
+						}
+				}
+			}
+
+			//ARRASTA//
+			if(evt.type == SDL_MOUSEMOTION){
+				SDL_GetMouseState(&novoX,&novoY);
+				if(r1.x <= 460  && r1.y <= 460 && selecionado){
+					if(estado_atual == CLICANDO){
+						estado_atual = ARRASTANDO;
+					}
+					posX = novoX + dx;
+					posY = novoY + dy;
+					r1.x = posX ;
+					r1.y = posY ;
+				}
 			}
 		}
-	}
 
-	switch (evt.type) {
-		case SDL_QUIT:
-			rodando = false;
-			break;
+		switch (evt.type) {
+			case SDL_QUIT:
+				rodando = false;
+				break;
+		}
+		}
 	}
-	}
-}
